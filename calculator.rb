126db99cf3859ba3ad3920bde7091d5a53afa5ea
Shervon Ceromain <shervonc@gmail.com>
2020-01-30 22:09:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@ first_number = 12 # Add your variables here
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
 qoutient = first_number / second_number
