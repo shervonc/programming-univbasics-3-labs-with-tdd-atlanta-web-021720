d800cbe962f5170f580a9e9fde9d3605890c246f
Shervon Ceromain <shervonc@gmail.com>
2020-01-30 22:08:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 first_number = 12 # Add your variables here
 second_number = 2
 sum = first_number + second_number
-difference
+difference = first_number - second_number
+qoutient = first_number / second_number
